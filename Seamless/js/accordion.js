6d6751bcf6bd0b26360298d391d0262d3ce5204f
Dave Dechant <davedechant@gmail.com>
2019-09-25 15:04:07 -0500

    Overall Accordion Updates

--- Seamless/js/accordion.js
+++ Seamless/js/accordion.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var accordion_content = this.nextElementSibling;
-    if (accordion_content.style.maxHeight){
-      accordion_content.style.maxHeight = null;
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      accordion_content.style.maxHeight = accordion_content.scrollHeight + "px";
-    } 
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
 }

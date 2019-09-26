8ff2331a771a87de4e041f0c2437aaa96089cdc7
Dave Dechant <davedechant@gmail.com>
2019-09-26 14:44:17 -0500

    Cleaned up CSS

--- Seamless/js/accordion-table.js
+++ Seamless/js/accordion-table.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("accordion_tab2");
+var acc = document.getElementsByClassName("accordion_tab_table");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var accordion_content2 = this.nextElementSibling;
-    if (accordion_content2.style.maxHeight){
-      accordion_content2.style.maxHeight = null;
+    var accordion_content_table = this.nextElementSibling;
+    if (accordion_content_table.style.maxHeight){
+      accordion_content_table.style.maxHeight = null;
     } else {
-      accordion_content2.style.maxHeight = accordion_content2.scrollHeight + "px";
+      accordion_content_table.style.maxHeight = accordion_content_table.scrollHeight + "px";
     } 
   });
   acc[0].click()

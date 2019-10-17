323522391f49a20ec699c90763ad89a6d1bb9936
Dave Dechant <davedechant@gmail.com>
2019-10-17 10:01:56 -0500

    Adding Tabs

--- Seamless/js/accordion-table-min.js
+++
@@ -1,1 +0,0 @@
-var i,acc=document.getElementsByClassName("accordion_tab_table");for(i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");var t=this.nextElementSibling;t.style.maxHeight?t.style.maxHeight=null:t.style.maxHeight=t.scrollHeight+"px"}),acc[0].click();
